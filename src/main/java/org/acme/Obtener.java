d1c286669f3fea09f7e4abf7379958a114ad1be9
Ënemyitachi <ofspsp10@hotmail.com>
2024-03-25 15:21:19 -0600

    Catalogos

--- src/main/java/org/acme/Obtener.java
+++ src/main/java/org/acme/Obtener.java
@@ -2,6 +2,7 @@ package org.acme;
 
 import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
@@ -20,10 +21,8 @@ public class Obtener {
     String USER = "sa";
     String PASS = "12345";
 
-    @GET
-    @Path("obtener/{tabla}")
-    public Response getDatos(@PathParam("tabla") String tabla) {
-        String query = "SELECT * FROM "+ tabla;
+
+    public Response getDatos(String query) {
         List<Map<String, String>> resultados = new ArrayList<>();
         Log.info(query);
 
@@ -48,4 +47,67 @@ public class Obtener {
         }
         return Response.ok(resultados).build();
     }
+
+    //GENERICOS
+    //Obtener la tabla sin claves foraneas
+    @GET
+    @Path("obtener/{catalogo}")
+    public Response obtener_Catalogo(@PathParam("catalogo") String catalogo){return getDatos("SELECT * FROM "+ catalogo);}
+
+    //Obtener catalogo desde con una llave foranea
+    @POST
+    @Path("obtener/{catalogo}")
+    public Response obtener_Catalogo_Desde_llave(@PathParam("catalogo") String catalogo, String llave_condicion, String valor_condicion){
+        return getDatos("SELECT * FROM "+ catalogo
+                + "WHERE "+ llave_condicion
+                + " = " + valor_condicion);
+    }
+
+    public Response obtener_Catalogo_Desde_llave(String catalogo, String llave_condicion1, String valor_condicion1,
+                                                 String llave_condicion2, String valor_condicion2){
+        return getDatos("SELECT * FROM "+ catalogo
+            + "WHERE "+ llave_condicion1
+            + " = " + valor_condicion1
+            + "AND " + llave_condicion2
+            + " = " + valor_condicion2);
+    }
+
+    public Response obtener_Catalogo_Desde_llave(String catalogo, String llave_condicion1, String valor_condicion1,
+                                                 String llave_condicion2, String valor_condicion2,
+                                                 String llave_condicion3, String valor_condicion3){
+        return getDatos("SELECT * FROM "+ catalogo
+            + "WHERE "+ llave_condicion1
+            + " = " + valor_condicion1
+            + "AND " + llave_condicion2
+            + " = " + valor_condicion2
+            + "AND " +llave_condicion3
+            + " = " + valor_condicion3
+        );
+    }
+
+    // OO metodo por metodo
+    public Response obtener_Cat_Pais(){return getDatos("SELECT * FROM CAT_PAIS");}
+
+    public Response obtener_Cat_Pais_Clave(){return getDatos("SELECT * FROM CAT_PAIS");}
+
+    public Response obtener_Cat_Entidad_Nacimiento(){return getDatos("SELECT * FROM CAT_PAIS");}
+
+    public Response obtener_Cat_Entidad_Domicilio(){return getDatos("SELECT * FROM CAT_PAIS");}
+
+    public Response obtener_Cat_Municipio_Desde_Entidad(String Entidad){return getDatos("SELECT * FROM CAT_PAIS WHERE ID_ENTIDAD_FEDERATIVA ="+Entidad);}
+
+    public Response obtener_Cat_Ciudad_Desde_Entidad(String Entidad){return getDatos("SELECT * FROM CAT_PAIS WHERE ID_ENTIDAD_FEDERATIVA ="+ Entidad);}
+
+    public Response obtener_Cat_Codigo_Postal_Desde_Entidad(String Entidad){return getDatos("SELECT * FROM CAT_PAIS WHERE ID_ENTIDAD_FEDERATIVA ="+ Entidad);}
+
+    public Response obtener_Cat_Codigo_Postal_Desde_Municipio(String Municipio){return getDatos("SELECT * FROM CAT_PAIS WHERE ID_MUNICIPIO ="+ Municipio);}
+
+    public Response obtener_Cat_Colonia(String CP, String Ciudad, String Entidad){return getDatos("SELECT * FROM CAT_PAIS WHERE ID_ENTIDAD_FEDERATIVA ="+ Entidad
+                                                                                                                +"AND ID_CODIGO_POSTAL ="+ CP
+                                                                                                                +"AND ID_CIUDAD ="+Ciudad);}
+
+    public Response obtener_Cat_Sector(){return getDatos("SELECT * FROM CAT_GIRO_SECTOR");}
+    public Response obtener_Cat_Giro_Desde_Sector(String id_Sector){return getDatos("SELECT * FROM CAT_GIRO_SUBSECTOR WHERE ID_SECTOR = "+id_Sector);}
+    public Response obtener_Cat_Giro_Rama_Desde_Subsector(String subSector){return getDatos("SELECT * FROM CAT_GIRO_RAMA WHERE ID_SUBSECTOR = "+subSector);}
+    public Response obtener_Cat_Giro_Subrama_Desde_Rama(String subSector){return getDatos("SELECT * FROM CAT_GIRO_RAMA WHERE ID_SUBSECTOR = "+subSector);}
 }
